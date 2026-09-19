310a33069bfe0b42617e6e5830461f68dbad5e05
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-09-19 19:53:22 +0100

    Initial Commit!

---
+++ src/ci3/date_lang.php
@@ -0,0 +1,75 @@
+<?php
+
+$lang['date_year'] = 'Rok';
+$lang['date_years'] = 'Roky';
+$lang['date_month'] = 'Mesiac';
+$lang['date_months'] = 'Mesiace';
+$lang['date_week'] = 'Týždeň';
+$lang['date_weeks'] = 'Týždne';
+$lang['date_day'] = 'Deň';
+$lang['date_days'] = 'Dni';
+$lang['date_hour'] = 'Hodina';
+$lang['date_hours'] = 'Hodiny';
+$lang['date_minute'] = 'Minúta';
+$lang['date_minutes'] = 'Minúty';
+$lang['date_second'] = 'Sekunda';
+$lang['date_seconds'] = 'Sekundy';
+
+$lang['date_just_now'] = 'práve teraz';
+$lang['date_seconds_ago'] = '=1{pred # sekundou} 2-4{pred # sekundami} other{pred # sekundami}';
+$lang['date_minutes_ago'] = '=1{pred # minútou} 2-4{pred # minútami} other{pred # minútami}';
+$lang['date_hours_ago'] = '=1{pred # hodinou} 2-4{pred # hodinami} other{pred # hodinami}';
+$lang['date_days_ago'] = '=1{včera} 2-4{pred # dňami} other{pred # dňami}';
+$lang['date_weeks_ago'] = '=1{pred # týždňom} 2-4{pred # týždňami} other{pred # týždňami}';
+$lang['date_months_ago'] = '=1{pred # mesiacom} 2-4{pred # mesiacmi} other{pred # mesiacmi}';
+$lang['date_years_ago'] = '=1{pred # rokom} 2-4{pred # rokmi} other{pred # rokmi}';
+
+$lang['date_su'] = 'Ne';
+$lang['date_mo'] = 'Po';
+$lang['date_tu'] = 'Ut';
+$lang['date_we'] = 'St';
+$lang['date_th'] = 'Št';
+$lang['date_fr'] = 'Pi';
+$lang['date_sa'] = 'So';
+
+$lang['date_sun'] = 'Ne';
+$lang['date_mon'] = 'Po';
+$lang['date_tue'] = 'Ut';
+$lang['date_wed'] = 'St';
+$lang['date_thu'] = 'Št';
+$lang['date_fri'] = 'Pi';
+$lang['date_sat'] = 'So';
+
+$lang['date_sunday'] = 'Nedeľa';
+$lang['date_monday'] = 'Pondelok';
+$lang['date_tuesday'] = 'Utorok';
+$lang['date_wednesday'] = 'Streda';
+$lang['date_thursday'] = 'Štvrtok';
+$lang['date_friday'] = 'Piatok';
+$lang['date_saturday'] = 'Sobota';
+
+$lang['date_jan'] = 'Jan';
+$lang['date_feb'] = 'Feb';
+$lang['date_mar'] = 'Mar';
+$lang['date_apr'] = 'Apr';
+$lang['date_may'] = 'Máj';
+$lang['date_jun'] = 'Jún';
+$lang['date_jul'] = 'Júl';
+$lang['date_aug'] = 'Aug';
+$lang['date_sep'] = 'Sep';
+$lang['date_oct'] = 'Okt';
+$lang['date_nov'] = 'Nov';
+$lang['date_dec'] = 'Dec';
+
+$lang['date_january'] = 'Január';
+$lang['date_february'] = 'Február';
+$lang['date_march'] = 'Marec';
+$lang['date_april'] = 'Apríl';
+$lang['date_mayl'] = 'Máj';
+$lang['date_june'] = 'Jún';
+$lang['date_july'] = 'Júl';
+$lang['date_august'] = 'August';
+$lang['date_september'] = 'September';
+$lang['date_october'] = 'Október';
+$lang['date_november'] = 'November';
+$lang['date_december'] = 'December';
---
+++ src/info.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * CiSkeleton Language Metadata - Slovak
+ *
+ * @package     CiSkeleton\Core
+ * @subpackage  Language
+ * @author      Kader Bouyakoub <bkader[at]mail[dot]com>
+ * @copyright   Copyright (c) 2018-present, Kader Bouyakoub
+ */
+
+return [
+	'name'       => 'Slovenčina',
+	'name_en'    => 'Slovak',
+	'locale'     => 'sk_SK',
+	'tag'        => 'sk-SK',
+	'direction'  => 'ltr',
+	'code'       => 'sk',
+	'flag'       => 'sk',
+	'version'    => '1.0.0',
+	'version_id' => 10000,
+
+	// Localized language names.
+	'localized_names' => [
+		'arabic'     => 'Arabčina',
+		'armenian'   => 'Arménčina',
+		'bengali'    => 'Bengálčina',
+		'brazilian'  => 'Portugalčina (Brazília)',
+		'bulgarian'  => 'Bulharčina',
+		'catalan'    => 'Katalánsky',
+		'chinese'    => 'Čínština (zjednodušená)',
+		'croatian'   => 'Chorvátčina',
+		'czech'      => 'Čeština',
+		'danish'     => 'Dánčina',
+		'dutch'      => 'Holandčina',
+		'english'    => 'Angličtina',
+		'estonian'   => 'Estónsky',
+		'filipino'   => 'Filipínčina',
+		'finnish'    => 'Fínčina',
+		'french'     => 'Francúzsky',
+		'georgian'   => 'Gruzínčina',
+		'german'     => 'Nemčina',
+		'greek'      => 'Gréčtina',
+		'hindi'      => 'Hindčina',
+		'hungarian'  => 'Maďarčina',
+		'indonesian' => 'Indonézština',
+		'italian'    => 'Taliančina',
+		'japanese'   => 'Japončina',
+		'korean'     => 'Kórejčina',
+		'latvian'    => 'Lotyština',
+		'lithuanian' => 'Litovčina',
+		'malay'      => 'Malajčina',
+		'norwegian'  => 'Nórčina',
+		'persian'    => 'Perzština',
+		'polish'     => 'Poľština',
+		'portuguese' => 'Portugalčina',
+		'romanian'   => 'Rumunčina',
+		'russian'    => 'Ruština',
+		'serbian'    => 'Srbčina',
+		'slovak'     => 'Slovenčina',
+		'slovenian'  => 'Slovinčina',
+		'spanish'    => 'Španielčina',
+		'swahili'    => 'Svahilčina',
+		'swedish'    => 'Švédčina',
+		'taiwanese'  => 'Čínština (tradičná)',
+		'tamil'      => 'Tamilčina',
+		'thai'       => 'Thajčina',
+		'turkish'    => 'Turečina',
+		'ukrainian'  => 'Ukrajinčina',
+		'urdu'       => 'Urdčina',
+		'vietnamese' => 'Vietnamčina',
+	],
+];
